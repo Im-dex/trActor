c70e3ab2fcd17ca7535ac15e56e54b163d0a5c19
a.tsukanov <tsukanov.anton@gmail.com>
2016-07-26 16:44:43 +0300

    Added TraversableLike extension method `split`

---
+++ tractor-core/src/main/scala/org/imdex/tractor/package.scala
@@ -0,0 +1,31 @@
+package org.imdex
+
+import scala.annotation.tailrec
+import scala.collection.{TraversableLike, mutable}
+import scala.collection.generic.CanBuildFrom
+import scala.language.higherKinds
+
+/**
+  * Created by a.tsukanov on 26.07.2016.
+  */
+package object tractor {
+    implicit class RichTractorTraversableLike[T, Collection[X] <: TraversableLike[X, Collection[X]]](val collection: Collection[T]) extends AnyVal {
+        def split(lengths: Int*)(implicit cbf: CanBuildFrom[Collection[T], Collection[T], Collection[Collection[T]]]): Collection[Collection[T]] = {
+            @tailrec
+            def split(lengths: Seq[Int], offset: Int = 0, builder: mutable.Builder[Collection[T], Collection[Collection[T]]] = cbf()): Collection[Collection[T]] = {
+                if (lengths.isEmpty) {
+                    val trailingPart = collection.splitAt(offset)._2
+                    if (trailingPart.nonEmpty) builder += trailingPart
+                    builder.result()
+                } else {
+                    val length = lengths.head
+                    val nextOffset = offset + length
+                    builder += collection.slice(offset, nextOffset)
+                    split(lengths.tail, nextOffset, builder)
+                }
+            }
+
+            split(lengths)
+        }
+    }
+}
---
+++ tractor-core/src/test/scala/org/imdex/tractor/TraversableLikeExtensionSpec.scala
@@ -0,0 +1,23 @@
+package org.imdex.tractor
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by a.tsukanov on 26.07.2016.
+  */
+class TraversableLikeExtensionSpec extends FlatSpec with Matchers {
+    "Extension method split" should "split container into `lengths.size` parts of corresponding length" in {
+        (1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil).split(2, 3) shouldBe List(
+            1 :: 2 :: Nil,
+            3 :: 4 :: 5 :: Nil,
+            6 :: Nil
+        )
+    }
+
+    "Extension method split" should "skip empty trailing part" in {
+        (1 :: 2 :: 3 :: 4 :: 5 :: Nil).split(2, 3) shouldBe List(
+            1 :: 2 :: Nil,
+            3 :: 4 :: 5 :: Nil
+        )
+    }
+}
